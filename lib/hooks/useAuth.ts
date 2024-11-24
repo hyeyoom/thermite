c1aeeb2c40fa75b799962beaf5db829c2ece78f3
hyeyoom <neoul_chw@icloud.com>
2024-11-24 23:12:53 +0900

    refactor: removed unused useAuth hook

--- lib/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { signOutServerAction } from '@/app/actions/auth.actions'
-
-export function useAuth() {
-  const signOut = async () => {
-    await signOutServerAction()
-  }
-
-  return { signOut }
-} 
